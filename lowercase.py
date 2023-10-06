c06137d7106ca570d10eca9a300ba008436fdd22
Sekhar420 <sekharchris132@gmail.com>
2023-10-07 00:51:07 +0530

    Lowercase first

---
+++ lowercase.py
@@ -0,0 +1,12 @@
+str1 = "PYnAtivE"
+print('Original String:', str1)
+lower = []
+upper = []
+for char in str1:
+    if char.islower():
+        lower.append(char)
+    else:
+        upper.append(char)
+
+sorted_str = ''.join(lower + upper)
+print('Result:', sorted_str)
